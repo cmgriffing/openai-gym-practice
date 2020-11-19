893cd45efe9f45ed8a30b9699a7dc42b27356a63
Chris Griffing <cmgriffing@gmail.com>
2020-11-19 15:03:55 -0800

    try larger memory and warmup

--- cem_carracing.py
+++ cem_carracing.py
@@ -55,7 +55,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 print(model.summary())
 
-memory = EpisodeParameterMemory(limit=100000, window_length=1)
+memory = EpisodeParameterMemory(limit=1000000, window_length=1)
 
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=10000)
 
@@ -64,7 +64,7 @@ policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., valu
 # cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
 # cem.compile()
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=2000,
                target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
